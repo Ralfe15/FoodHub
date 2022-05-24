6f95e9f7c7777b597e688ef400547c81293f2d52
Igor Liberato <igcbsb@gmail.com>
2022-05-24 18:48:42 +0100

    Deleted comment

--- templates/images.tpl.php
+++ templates/images.tpl.php
@@ -15,34 +15,3 @@
       </a>
     </article>
 <?php }?>
-
-
-<?php function drawUploadImage() {
-  /*
-  $db = getDatabaseConnection();
-
-  $stmt = $db->prepare("SELECT * FROM Img ORDER BY idImage DESC");
-  $stmt->execute();
-  
-  $images = $stmt->fetchAll();
-?>
-    <nav>
-      <form action="upload.php" method="post" enctype="multipart/form-data">
-        <label>Title:
-          <input type="text" name="title">
-        </label>
-        <input type="file" name="image">
-        <input type="submit" value="Upload">
-      </form>
-    </nav>
-    <section id="images">
-      <?php foreach ($images as $image) { ?>
-      <article class="image">
-        <header><h2><?=$image['title']?></h2></header>
-        <a href="../pages/view_image.php?id=<?=$image['id']?>">
-          <img src="../images/<?=$image['id']?>.jpg" width="200" height="200">
-        </a>
-      </article>
-      <?php } ?>
-    </section>
-<?php */}  ?>
