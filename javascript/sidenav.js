8a49607dc2ef85428cf55f6e404be00d358b1690
Rafael Schmidt <ralfe.schmidt@gmail.com>
2022-05-31 15:25:35 +0100

    sidenav cart working

--- javascript/sidenav.js
+++ javascript/sidenav.js
@@ -1,6 +1,8 @@
 function openNav() {
     const badge = document.querySelector("#badge");
     const total = document.querySelector("#total");
+    const itemslist = document.querySelector(".cartItems");
+    itemslist.innerHTML = '';
     var total_val = 0;
     const keys = Object.keys(window.sessionStorage);
     badge.textContent = keys.length;
@@ -8,7 +10,7 @@ function openNav() {
         const dish = JSON.parse(key); 
         const ammount = window.sessionStorage.getItem(key);
         total_val += parseInt(dish.price) * parseInt(ammount);
-        // assembleCartItem(dish, ammount);
+        assembleCartItem(dish, ammount);
     }
     total.textContent = "$" +total_val+",00";
     document.getElementById("mySidenav").style.width = "350px";
@@ -26,8 +28,9 @@ function openNav() {
     quantity.className = "item-quantity";
     img.src = 'https://picsum.photos/200/200?business'
     name.textContent = capitalizeFirstLetter(dish.name);
-    price.textContent = dish.price;
-    quantity.textContent = ammount;
+    price.textContent = "$" + dish.price + ",00";
+    quantity.textContent = "X " + ammount;
+    quantity.id = dish.name;
     itemBox.appendChild(img);
     itemBox.appendChild(name);
     itemBox.appendChild(price);
@@ -43,13 +46,24 @@ function openNav() {
     const key = JSON.stringify(dish)
     if(window.sessionStorage.getItem(key) == null){
         window.sessionStorage.setItem(key, "1");
-        assembleCartItem(dish, 1);
+        assembleCartItem(dish, 1)
     }
     else{
         window.sessionStorage.setItem(key, parseInt(window.sessionStorage.getItem(key))+1)
+        const ammount = document.querySelector('#' + dish.name);
+        ammount.textContent = "X " + window.sessionStorage.getItem(key);
     }
+    // const keys = Object.keys(window.sessionStorage);
+    // for(const key of keys){
+    //   const dish = JSON.parse(key); 
+    //   assembleCartItem(dish, window.sessionStorage.getItem(key))
+    // }
     openNav();
   }
+
+
+
+
 
   function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
