b587a484714ecd166d171ec16a300f21e9591d73
Rafael Schmidt <ralfe.schmidt@gmail.com>
2022-06-14 23:50:36 +0100

    rating search

--- pages/search.php
+++ pages/search.php
@@ -23,6 +23,15 @@ if ($_POST['search-type'] == "dish") {
     $stmt->execute(array($res));
     $results = $stmt->fetchAll();
 }
+if ($_POST['search-type'] == "rating") {
+    $res = $_POST['search'];
+    $stmt = $db->prepare('Select restaurant.idrestaurant, restaurant.name, restaurant.category, restaurant.address, restaurant.logo, review.idRestaurant, avg(rating) as rating from review join restaurant on restaurant.idRestaurant = review.idRestaurant where rating >= ?');
+    $stmt->execute(array(intval($res)));
+    $results = $stmt->fetchAll();
+    if($results[0]['rating'] == null){
+        $results = [];
+    }
+}
 ?>
 
 <?= drawHeader(); ?>
@@ -48,6 +57,8 @@ if ($_POST['search-type'] == "dish") {
         <span>By:</span>
         <input type="radio" name="select" id="option-1" value="restaurant" <?php if ($_POST['search-type'] == "restaurant") echo 'checked' ?> onclick="toggleSearch()">
         <input type="radio" name="select" id="option-2" value="dish" <?php if ($_POST['search-type'] == "dish") echo 'checked' ?> onclick="toggleSearch()">
+        <input type="radio" name="select" id="option-3" value="rating" <?php if ($_POST['search-type'] == "rating") echo 'checked' ?> onclick="toggleSearch()">
+
         <label for="option-1" class="option option-1" name="restaurant">
             <div class="dot"></div>
             <span>Restaurant</span>
@@ -55,6 +66,10 @@ if ($_POST['search-type'] == "dish") {
         <label for="option-2" class="option option-2" name="dish">
             <div class="dot"></div>
             <span>Dish</span>
+        </label>
+        <label for="option-3" class="option option-3" name="rating">
+            <div class="dot"></div>
+            <span>Rating</span>
         </label>
     </div>
     <main>
